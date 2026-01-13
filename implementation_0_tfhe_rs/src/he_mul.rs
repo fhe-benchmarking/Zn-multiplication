919c92b0797fff31df32c12d9bd300f3140b047b
Florent Michel <florent.m42@gmail.com>
2026-01-13 17:39:27 +0000

    delete un-necessary imports

--- implementation_0_tfhe_rs/src/he_mul.rs
+++ implementation_0_tfhe_rs/src/he_mul.rs
@@ -4,9 +4,7 @@
 // This software is licensed under the terms of the Apache v2 License.
 // See the LICENSE.md file for details.
 
-use tfhe::prelude::*;
 use tfhe::FheUint64;
-use std::ops::{ Shr, Shl, BitAnd };
 
 pub fn half_cipher_cipher_mul_64(a: &FheUint64, b: &FheUint64) -> FheUint64 
 {
